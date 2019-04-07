a5fc60c19f5f00530ee9ed2bf988199e6109a3f0
Edison <ed_is_on@msn.com>
2019-04-07 23:37:10 +1000

    triedcanvas

---
+++ Assets/Programming Class/Scripts/CanvasDialogue.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasDialogue : MonoBehaviour {
+    public string[] dlgText;
+    public int index, optionsIndex;
+    private Text currentText;
+    private GameObject back;
+    private GameObject next;
+    private Button backB;
+    private Button nextB;
+
+    // Use this for initialization
+    void Start () {
+       currentText = this.GetComponent<Text>();
+        back = transform.Find("Back").gameObject;
+        next = transform.Find("Next").gameObject;
+        backB = back.GetComponent<Button>();
+        nextB = next.GetComponent<Button>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        currentText.text = dlgText[index];
+
+        if (index == 0)
+        {
+            back.SetActive(false);
+        }
+        else if (index < dlgText.Length -1)
+        {
+          back.SetActive(true);
+            Debug.Log(dlgText.Length);
+
+        }
+
+        else if (index == dlgText.Length)
+        {
+
+            Text text = next.GetComponentInChildren<Text>();
+            text.text = "DONE";
+
+        
+
+
+        }
+
+
+	}
+
+    public void Next()
+    {
+        index += 1; 
+    }
+
+    public void Back()
+    {
+        index -= 1;
+    }
+}
